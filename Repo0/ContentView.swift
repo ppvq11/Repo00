075b41026d4b7eb386a6d606da0dfc405036d317
Muna <muai046@gmail.com>
2024-07-23 20:55:17 +0300

    Initial Commit

---
+++ Repo0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Repo0
+//
+//  Created by Muna Aiman Al-hajj on 17/01/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Repo0/Repo0App.swift
@@ -0,0 +1,17 @@
+//
+//  Repo0App.swift
+//  Repo0
+//
+//  Created by Muna Aiman Al-hajj on 17/01/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Repo0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
